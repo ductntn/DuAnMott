7723d4076cda001ce50e3e04ecd5d6a4754eb3ec
ductn <ngocduc0607@gmail.com>
2020-11-20 12:58:14 +0700

    Initial commit

---
+++ app/src/main/java/com/example/duanmot/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.duanmot;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import com.example.duanmot.Product.ListProduct;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void sanPham(View view) {
+        Intent intent = new Intent(MainActivity.this, ListProduct.class);
+        startActivity(intent);
+    }
+
+    public void donHang(View view) {
+    }
+
+    public void doanhThu(View view) {
+    }
+
+    public void khachHang(View view) {
+    }
+}
